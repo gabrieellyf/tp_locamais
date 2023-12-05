ede11cecfef584d81eedd58bbc54db61abff387f
art1544 <141589021+art1544@users.noreply.github.com>
2023-12-05 01:39:20 -0300

    Add files via upload

---
+++ codigo/funcoes.h
@@ -0,0 +1,8 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+// Aqui você deve declarar as funções que serão implementadas em funcoes.c
+
+void iniciarSistema();
+
+#endif // FUNCOES_H
---
+++ codigo/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "funcoes.h"
+
+int main() {
+    // Aqui você pode começar a interagir com as funções do seu projeto
+    // Exemplo de uso:
+    iniciarSistema();
+
+    return 0;
+}
